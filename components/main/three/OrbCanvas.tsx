9e8bc41085063eba7f7f9aab1c367e971f91b0c6
Siam Rahman <siamr.bro99@gmail.com>
2022-12-21 01:40:35 -0500

    Orb plops down on initial render

--- components/main/three/OrbCanvas.tsx
+++ components/main/three/OrbCanvas.tsx
@@ -1,16 +1,22 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Orb from "./Orb";
+import { motion } from "framer-motion";
 
 const OrbCanvas = () => {
   return (
-    <div className="hidden sm:inline-flex w-full h-full absolute">
-      <Canvas camera={{position: [0, 0, 8]}}>
+    <motion.div
+      className="hidden sm:inline-flex w-full h-full absolute"
+      initial={{ opacity: 0, y: "-100vh" }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 3, delay: 7, type: "spring", damping: 4 }}
+    >
+      <Canvas camera={{ position: [0, 0, 8] }}>
         <Suspense fallback={null}>
           <Orb />
         </Suspense>
       </Canvas>
-    </div>
+    </motion.div>
   );
 };
 
