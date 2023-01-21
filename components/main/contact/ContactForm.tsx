837f3c0e6f57bf194ca38ead683d6f43fed512d5
Siam Rahman <siamr.bro99@gmail.com>
2023-01-21 16:49:40 -0500

    Upgraded to next 13

--- components/main/contact/ContactForm.tsx
+++ components/main/contact/ContactForm.tsx
@@ -78,31 +78,37 @@ const ContactForm = () => {
     >
       <FormCircles />
       <div className="flex space-x-2">
+        <div className="w-1/2">
+          <input
+            type="text"
+            className="w-full contact-form"
+            placeholder="First Name"
+            {...register("firstName", {
+              required: true,
+              pattern: /^[a-z]+$/gi,
+            })}
+          />
+        </div>
+        <div className="w-1/2">
+          <input
+            type="text"
+            className="w-full contact-form"
+            placeholder="Last Name"
+            {...register("lastName", { pattern: /^[a-z]+$/gi })}
+          />
+        </div>
+      </div>
+      <div>
         <input
-          type="text"
-          className="w-1/2 contact-form"
-          placeholder="First Name"
-          {...register("firstName", {
+          type="email"
+          className="w-full contact-form"
+          placeholder="Email"
+          {...register("email", {
             required: true,
-            pattern: /^[a-z]+$/gi,
+            pattern: /.+(@gmail)\.(?=com)/g,
           })}
         />
-        <input
-          type="text"
-          className="w-1/2 contact-form"
-          placeholder="Last Name"
-          {...register("lastName", { pattern: /^[a-z]+$/gi })}
-        />
       </div>
-      <input
-        type="email"
-        className="contact-form"
-        placeholder="Email"
-        {...register("email", {
-          required: true,
-          pattern: /.+(@gmail)\.(?=com)/g,
-        })}
-      />
       <textarea
         className="z-10 h-80 md:w-[700px] lg:w-[52vw] xl:w-[700px] resize-none outline-none p-5 font-ui text-center text-xl sm:text-2xl sm:shadow-dimmer dark:sm:shadow-dim dark:bg-[#272727] bg-zinc-100"
         placeholder="Message"
@@ -134,5 +140,71 @@ const ContactForm = () => {
     </motion.form>
   );
 };
+
+// <motion.form
+//   initial={{ opacity: 0 }}
+//   whileInView={{ opacity: 1 }}
+//   viewport={{ once: true }}
+//   transition={{ duration: 1 }}
+//   className="relative flex flex-col space-y-2 w-fit"
+//   onSubmit={handleSubmit(onSubmit, onError)}
+//   ref={formRef}
+// >
+//   <FormCircles />
+//   <div className="flex space-x-2">
+//     <input
+//       type="text"
+//       className="w-1/2 contact-form"
+//       placeholder="First Name"
+//       {...register("firstName", {
+//         required: true,
+//         pattern: /^[a-z]+$/gi,
+//       })}
+//     />
+//     <input
+//       type="text"
+//       className="w-1/2 contact-form"
+//       placeholder="Last Name"
+//       {...register("lastName", { pattern: /^[a-z]+$/gi })}
+//     />
+//   </div>
+//   <input
+//     type="email"
+//     className="contact-form"
+//     placeholder="Email"
+//     {...register("email", {
+//       required: true,
+//       pattern: /.+(@gmail)\.(?=com)/g,
+//     })}
+//   />
+//   <textarea
+//     className="z-10 h-80 md:w-[700px] lg:w-[52vw] xl:w-[700px] resize-none outline-none p-5 font-ui text-center text-xl sm:text-2xl sm:shadow-dimmer dark:sm:shadow-dim dark:bg-[#272727] bg-zinc-100"
+//     placeholder="Message"
+//     {...register("message", { required: true })}
+//   />
+//   <button
+//     type="submit"
+//     className="p-4 text-gray-500 dark:text-gray-400 font-ui text-xl sm:text-2xl sm:shadow-dimmer dark:sm:shadow-dim dark:bg-[#272727] bg-zinc-100 active:scale-95 transiton duration-200 ease-out"
+//   >
+//     Send
+//   </button>
+//   <AnimatePresence mode="wait">
+//     {!catClicked ? (
+//       <motion.div
+//         className="hidden sm:inline-flex absolute bottom-[40%] -left-[7%] -rotate-[90deg] cursor-pointer"
+//         whileHover={{ x: -82 }}
+//         animate={{ rotate: -90 }}
+//         onClick={() => setCatClicked(true)}
+//         exit={{ opacity: 0 }}
+//         transition={{
+//           x: { duration: 1, ease: "anticipate" },
+//           opacity: { duration: 3 },
+//         }}
+//       >
+//         <Image alt="cat" src={cat} width={150} height={150} />
+//       </motion.div>
+//     ) : null}
+//   </AnimatePresence>
+// </motion.form>
 
 export default ContactForm;
